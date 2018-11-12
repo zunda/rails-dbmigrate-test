4bd166ce14dc599fe33e22c4ba3dbeb00f18cd47
zunda <zundan@gmail.com>
2018-11-11 14:27:31 -1000

    Make it possible to update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -20,8 +25,18 @@ class ArticlesController < ApplicationController
     end
   end
 
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
