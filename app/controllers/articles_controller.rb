84d7d4f4775909be9569286f4e5e57ae3d0424a8
zunda <zundan@gmail.com>
2018-11-11 14:16:32 -1000

    Verify title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 private
