ee65825e56a508828e08f8bbe54d9404507d3fed
zunda <zundan@gmail.com>
2018-11-14 17:22:26 -1000

    Add body column and sync it from text column

---
+++ db/migrate/20181115023021_add_body_to_articles.rb
@@ -0,0 +1,28 @@
+class AddBodyToArticles < ActiveRecord::Migration[5.2]
+  def up
+    add_column :articles, :body, :text
+		execute <<_ADD_TRIGGER
+UPDATE articles SET body = text;
+CREATE OR REPLACE FUNCTION sync_to_body()
+	RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.body := NEW.text;
+    RETURN NEW;
+  END;
+  $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER sync_to_body_trigger
+  BEFORE INSERT OR UPDATE OF text ON articles
+  FOR EACH ROW EXECUTE PROCEDURE sync_to_body();
+_ADD_TRIGGER
+  end
+
+  def down
+    execute <<_REMOVE_TRIGGER
+DROP TRIGGER sync_to_body_trigger ON articles;
+DROP FUNCTION sync_to_body();
+UPDATE articles SET text = body;
+_REMOVE_TRIGGER
+    remove_column :articles, :body
+  end
+end
