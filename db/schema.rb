79f2f977b3fce0338892f6d5f9e4c375db51edc8
zunda <zundan@gmail.com>
2018-11-15 17:04:27 -1000

    Copy to body column only when it is null

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_023021) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 2018_11_11_020702) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "body"
   end
 
 end
